49bf694cb7c50b9ae8259f69605a848083adc1da
Pavel SE <de.pavel.se@yandex.ru>
2020-10-05 16:13:21 +0300

    added layout

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+$team = [
+    ['name' => 'Hodor', 'position' => 'programmer'],
+    ['name' => 'Joker', 'position' => 'CEO'],
+    ['name' => 'Elvis', 'position' => 'CTO'],
+];
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () use ($team) {
+    // BEGIN (write your solution here)
+    return view('about', ['team' => $team]);
+    // END
 });
