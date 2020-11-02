8d5de2aebc6d796040d47fb11bde97145cbf94d1
pavel-else <de.pavel.se@yandex.ru>
2020-11-02 21:58:01 +0300

    api: added test route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//https://code.tutsplus.com/ru/tutorials/build-a-react-app-with-laravel-restful-backend-part-1-laravel-5-api--cms-29442
+Route::get('products', function () {
+    return response(['Product 1', 'Product 2', 'Product 3'], 200);
+});
