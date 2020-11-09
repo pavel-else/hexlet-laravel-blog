1e88a8e3fe911c43bea04c6a9c95728ec07a3a7e
pavel-else <de.pavel.se@yandex.ru>
2020-11-09 17:06:47 +0300

    Articles:store

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -20,4 +20,26 @@ class ArticleController extends Controller
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
     }
+
+    public function create()
+    {
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:10',
+        ]);
+
+        $article = new Article();
+        $article->fill($data);
+
+        $article->save();
+        $request->session()->flash('status', 'Task was successful!');
+        return redirect()
+            ->route('articles.index');
+    }
 }
